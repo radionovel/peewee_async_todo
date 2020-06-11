5f1c51632d67e09a446d41b665a73e25b702d01c
Andrey Radionov <a.radionov@animarender.com>
2020-06-11 10:29:57 +0500

    gitlab-ci

--- test/config.py
+++ test/config.py
@@ -1,11 +1,11 @@
 import os
 
 host = os.getenv('MYSQL_HOST') or 'localhost'
-port = os.getenv('MYSQL_PORT') or 3306
+port = os.getenv('MYSQL_PORT') or '33006'
 
 config = {
     'host': host,
-    'port': port,
+    'port': int(port),
     'database': 'app_test',
     'user': 'root',
     'password': 'secret'
